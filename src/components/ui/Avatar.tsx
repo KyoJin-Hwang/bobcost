a8517824a76ea8db809b78c4a3f9cec9dd773e6e
owen <rywls4248@gmail.com>
2024-12-28 18:56:09 +0900

    feat : header left 완료

--- src/components/ui/Avatar.tsx
+++ src/components/ui/Avatar.tsx
@@ -1,7 +1,7 @@
 const Avatar = () => {
   return (
     <img
-      className='h-[2.5rem] w-[2.5rem] rounded-full transition-all duration-200 hover:scale-[1.1]'
+      className='h-[2.5rem] w-[2.5rem] rounded-full transition-all duration-200 group-hover:scale-[1.1]'
       src='image/my.png'
       alt='Rounded avatar'
     />
