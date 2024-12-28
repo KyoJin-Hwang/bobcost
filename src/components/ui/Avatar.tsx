01e2f9491639ef0cf515f05c69dd63cfe54f52cb
owen <rywls4248@gmail.com>
2024-12-28 18:03:02 +0900

    feat : 아바타 컴포넌트 생성

---
+++ src/components/ui/Avatar.tsx
@@ -0,0 +1,11 @@
+const Avatar = () => {
+  return (
+    <img
+      className='h-[2.5rem] w-[2.5rem] rounded-full transition-all duration-200 hover:scale-[1.1]'
+      src='image/my.png'
+      alt='Rounded avatar'
+    />
+  );
+};
+
+export default Avatar;
