e70cabef07e82890e54e56e7c03cbd0d7206d55f
owen <owen1127@naver.com>
2024-12-28 18:27:48 +0900

    fix : header provider 안에추가
    header 가 provider 안에 안들어가서
    에러

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,12 +1,31 @@
 'use client';
 
-import Button from '@/components/Button';
-import { HoverText, Text } from '@/components/Text';
+import ThemeSwitch from './theme/Switch';
+import Avatar from '@/components/ui/Avatar';
+import Button from '@/components/ui/Button';
+import { Text } from '@/components/ui/Text';
+
+const HeaderLeft = () => {
+  return (
+    <div className='flex gap-2'>
+      <Avatar />
+      <div className='flex flex-col'>
+        <Text text='BOBcost' fontWeight='bold' fontSize={20} />
+        <Text text='Dev' fontWeight='bold' fontSize={20} />
+      </div>
+    </div>
+  );
+};
+
+const HeaderRight = () => {
+  return <ThemeSwitch />;
+};
 
 const Header = () => {
   return (
-    <header>
-      <Text text='BOBcost' fontWeight='bold' />
+    <header className='flex items-center justify-between border-b-2 border-b-[#121212] px-5 py-2.5 dark:border-b-white'>
+      <HeaderLeft />
+      <HeaderRight />
     </header>
   );
 };
