833c90cda67316e3a77f175b56778b0df3651b38
owen <rywls4248@gmail.com>
2025-01-05 19:35:51 +0900

    feat : post tpye 및 mdx 관련함수 제작
    mdx 관련 함수 제작
    1. mdx 개요파싱
    2. mdx 파일조회
    3. mdx Detail
    4. 날짜 최신순 배열정리
    5. 모든 포스트 목록 조회

--- src/app/blog/(main)/page.tsx
+++ src/app/blog/(main)/page.tsx
@@ -1,4 +1,12 @@
-const Blog = () => {
+import { getSortedPostList } from '@/lib/post';
+
+type Props = {
+  params: { category: string };
+};
+
+const Blog = async ({ params }: Props) => {
+  const postList = await getSortedPostList(params.category);
+  console.log(postList);
   return (
     <div className=''>
       <div>blog </div>
