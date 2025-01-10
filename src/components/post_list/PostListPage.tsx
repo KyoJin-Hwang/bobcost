1a9be69457b1a4e0cecc72043edf23a2fa7c0a5f
owen <rywls4248@gmail.com>
2025-01-10 23:03:58 +0900

    feat : mobile list

--- src/components/post_list/PostListPage.tsx
+++ src/components/post_list/PostListPage.tsx
@@ -1,4 +1,5 @@
 import CategoryList from './CategoryList';
+import PostCard from './PostCard';
 import {
   getAllPostCount,
   getCategoryDetailList,
@@ -13,7 +14,7 @@ const PostListPage = async ({ category }: PostListProps) => {
   const postList = await getSortedPostList(category);
   const categoryList = await getCategoryDetailList();
   const allPostCount = await getAllPostCount();
-
+  console.log(postList);
   return (
     <section className='mt-5 px-5'>
       <CategoryList
@@ -21,6 +22,11 @@ const PostListPage = async ({ category }: PostListProps) => {
         categoryList={categoryList}
         currentCategory={category}
       />
+      <section className='grid-cols-listGrid mt-4 grid gap-x-2 gap-y-4'>
+        {postList.map((post) => (
+          <PostCard key={post.url + post.createdAt} post={post} />
+        ))}
+      </section>
     </section>
   );
 };
